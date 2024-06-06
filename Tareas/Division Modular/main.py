b831aeacdb2d9c4e47f5dfd2d6c368e610a97500
omardtl24 <otoledo@unal.edu.co>
2024-06-05 21:53:15 -0500

    Division code task

---
+++ Tareas/Division Modular/main.py
@@ -0,0 +1,19 @@
+
+def extended_gcd(a, b):
+  if b == 0:
+    d, x, y = a, 1, 0
+  else:
+    (d, p, q) = extended_gcd(b, a % b)
+    x = q
+    y = p - q * (a // b)
+
+  assert a % d == 0 and b % d == 0
+  assert d == a * x + b * y
+  return (d, x, y)
+
+def divide(a, b, n):  #b/a mod n
+  d,s,t = extended_gcd(a, n)
+  return (s*b)%n
+  
+a, b, n = map(int, input().split());
+print(divide(a, b, n))
